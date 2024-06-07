688f2bd443aa7b904a1eba7feacef87fdc5a18a7
ahmedrefat21 <ahmedref21@gmail.com>
2024-06-07 23:21:20 +0300

    fix Response Model

--- Shopify/Shopify/Model/Response.swift
+++ Shopify/Shopify/Model/Response.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct Response : Codable{
     let smartCollections: [SmartCollections]?
-    let addresses: [Address] 
+    let addresses: [Address]
     let products: [Product]?
     let product: Product?
     
     private enum CodingKeys: String, CodingKey {
         case smartCollections = "smart_collections"
-        case addresses = "addresses"
+        case addresses
         case products = "products"
         case product = "product"
         
