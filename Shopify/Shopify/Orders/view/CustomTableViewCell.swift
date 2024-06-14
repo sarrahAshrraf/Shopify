0f9642d1243ddb4916b180fb645bc682412739bf
Sarrah Ashraf <154979941+sarrahAshrraf@users.noreply.github.com>
2024-06-14 16:03:28 -0400

    Revert "Orders"

--- Shopify/Shopify/Orders/view/CustomTableViewCell.swift
+++ Shopify/Shopify/Orders/view/CustomTableViewCell.swift
@@ -11,14 +11,13 @@ class CustomTableViewCell: UITableViewCell {
     
     static let identifier = "CustomTableViewCell"
     
-
-    @IBOutlet weak var orderName: UILabel!
+    @IBOutlet weak var orderImage: UIImageView!
     
-    @IBOutlet weak var numOfItems: UILabel!
+    
+    @IBOutlet weak var orderName: UILabel!
     
     @IBOutlet weak var orderPrice: UILabel!
     
-    @IBOutlet weak var orderStatus: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -28,14 +27,6 @@ class CustomTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-    }
-    
-    func setOrderValues(order: Orders){
-        self.orderName.text = order.name
-        self.numOfItems.text = order.customer?.createdAt
-        self.orderPrice.text = order.totalPrice
-        
-        self.orderStatus.text = order.financialStatus
     }
     
 }
