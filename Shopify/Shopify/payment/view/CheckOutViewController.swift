cee2c49b04b31a31d96b2b74528a43b025cec7b3
sarrahAshrraf <154979941+sarrahAshrraf@users.noreply.github.com>
2024-06-18 17:31:13 -0400

    done deceasing variants after post order

--- Shopify/Shopify/payment/view/CheckOutViewController.swift
+++ Shopify/Shopify/payment/view/CheckOutViewController.swift
@@ -79,7 +79,7 @@ class CheckOutViewController: UIViewController , AddressSelectionDelegate{
             checkOutVM.postOrder(order: order)
             print("order cuurencyyyyyyyyyyyyyyy")
             print(UserDefaults.standard.string(forKey: Constants.CURRENCY_KEY))
-            
+//            checkOutVM.updateVariantAfterPostOrder()
             print(order)
             
         
