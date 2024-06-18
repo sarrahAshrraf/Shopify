add8c33415a7e3fdbca39faea02ddbd513b2e998
ahmedrefat21 <ahmedref21@gmail.com>
2024-06-19 00:33:42 +0300

    add Color Collection View Cell in Product Details

---
+++ Shopify/Shopify/Model/Review.swift
@@ -0,0 +1,16 @@
+//
+//  Review.swift
+//  Shopify
+//
+//  Created by Ahmed Refat on 17/06/2024.
+//
+
+import Foundation
+
+
+struct Review {
+    let photo: String
+    let personName: String
+    let rate: Double
+    let reviewMessage: String
+}
