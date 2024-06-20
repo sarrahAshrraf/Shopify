4b09b7ea509aa8464577f5f744ddbe5ba30e3064
mohamedelghaish <kotbm055@gmail.com>
2024-06-20 22:34:10 +0300

    delete commented code

--- Shopify/Shopify/Category/view/CategoryCollectionViewCell.swift
+++ Shopify/Shopify/Category/view/CategoryCollectionViewCell.swift
@@ -36,17 +36,10 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         }
     }
     func setValues(product:Product, isFav: Bool) {
-//        print("Success2")
         self.product = product
         self.productName.text = product.title ?? ""
         self.productImage.kf.setImage(with: URL(string: product.image?.src ?? ""),
                                       placeholder: UIImage(named: Constants.noImage))
-//        if let variant = product.variants?.first {
-//            if let price = Double(variant.price) {
-//                let convertedPrice = price * currencyRate
-//                self.productPrice.text = String(format: "%.2f %@", convertedPrice, currencySymbol)
-//            }
-//        }
         if isFav {
             self.favBtn.setImage(UIImage(systemName: Constants.fillHeart), for: .normal)
         } else {
@@ -72,12 +65,3 @@ class CategoryCollectionViewCell: UICollectionViewCell {
         
     }
 }
-//        if product.variants?.count ?? 0 > 0{
-//            print(product.variants![0].price)
-//            self.productPrice.text = product.variants![0].price
-//        }
-//        if product.variants?.count ?? 0 > 0{
-////            String(format: "%.1f", Double(product.variants![0].price)! *  Constants)
-//
-//            self.productPrice.text = String(format: "%.1f", Double(product.variants![0].price)! )
-//        }
