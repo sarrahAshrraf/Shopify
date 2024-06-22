c257857b77ed0905f29d3fe12fa816cb414604ed
mohamedelghaish <kotbm055@gmail.com>
2024-06-22 22:08:09 +0300

    add function to test post data to api

--- Shopify/ShopifyTests/ShopifyTests.swift
+++ Shopify/ShopifyTests/ShopifyTests.swift
@@ -30,5 +30,61 @@ final class ShopifyTests: XCTestCase {
            }
            wait(for: [expectation], timeout: 20)
        }
+    
+    func testPostData() {
+        let url = URLs.shared.getAddressURL()
+        let expectation = XCTestExpectation(description: "postData completion handler called")
+
+        let address = Address(
+                    id: nil,
+                    customer_id: nil,
+                    name: "John Doe",
+                    first_name: "John",
+                    last_name: "Doe",
+                    phone: "123-456-7890",
+                    company: "ACME Corp",
+                    address1: "123 Main St",
+                    address2: "Apt 4",
+                    city: "Anytown",
+                    province: "Anyprovince",
+                    country: "Anycountry",
+                    zip: "12345",
+                    province_code: "AP",
+                    country_code: "AC",
+                    country_name: "Anycountry",
+                    default: true
+                )
+        let addressDict: [String: Any] = [
+            "address1": address.address1 ?? "",
+            "address2": address.address2 ?? "",
+            "city": address.city ?? "",
+            "company": address.company ?? "",
+            "first_name": address.first_name ?? "",
+            "last_name": address.last_name ?? "",
+            "phone": address.phone ?? "",
+            "province": address.province ?? "",
+            "country": address.country ?? "",
+            "zip": address.zip ?? "",
+            "name": address.name ?? "",
+            "province_code": address.province_code ?? "",
+            "country_code": address.country_code ?? "",
+            "country_name": address.country_name ?? "",
+            "default": address.default ?? false
+        ]
+        let finalDict: [String: Any] = ["address": addressDict]
+
+        NetworkManger.shared.postData(path: url , parameters: finalDict) { response, statusCode in
+            
+            if let statusCode = statusCode, (200...299).contains(statusCode) {
+                XCTAssert(true)
+            } else {
+                print("failed")
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 20)
+    }
 
 }
