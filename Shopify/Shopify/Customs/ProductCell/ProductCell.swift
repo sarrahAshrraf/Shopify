025a49ad1ea32486f3e5714d43d3bb57ec6f33b7
ahmedrefat21 <ahmedref21@gmail.com>
2024-06-22 10:20:15 +0300

    set currency rate in product cell

--- Shopify/Shopify/Customs/ProductCell/ProductCell.swift
+++ Shopify/Shopify/Customs/ProductCell/ProductCell.swift
@@ -19,9 +19,13 @@ class ProductCell: UITableViewCell {
     
     var localProduct: LocalProduct!
     var product: Product!
+    let defaults = UserDefaults.standard
+    var currencyRate: Double = 1.0
+    var currencySymbol: String = "USD"
     override func awakeFromNib() {
         super.awakeFromNib()
         configureContainerView()
+        setCurrency()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -43,8 +47,10 @@ class ProductCell: UITableViewCell {
         productBrandLabel.text = Splitter().splitBrand(text: product.title ?? "", delimiter: "| ")
         productImageView.kf.setImage(with: URL(string: product.image?.src ?? ""))
         productNameLabel.text = Splitter().splitName(text: product.title ?? "", delimiter: "| ")
-        print(product.title)
-        productPriceLabel.text = product.variants?[0].price ?? ""
+        let price = Int((Double((product.variants?[0].price)! ) ?? 0.0)*currencyRate)
+        productPriceLabel.text = String(price)
+        curencyLabel.text = currencySymbol
+    
     }
     
     private func configureContainerView() {
@@ -58,6 +64,14 @@ class ProductCell: UITableViewCell {
         containerView.layer.shadowOpacity = 0.2
         containerView.layer.shadowRadius = 5
         containerView.layer.cornerRadius = 20
+    }
+    
+    func setCurrency(){
+        if let rate = defaults.value(forKey: Constants.CURRENCY_VALUE) as? Double {
+            currencyRate = rate
+        }
+        let symbol = defaults.string(forKey: Constants.CURRENCY_KEY) ?? "USD"
+            currencySymbol = symbol
     }
     
 }
