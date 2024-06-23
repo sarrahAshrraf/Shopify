0e31830d32ebada5c5d0d2f12b18160e030e0007
mohamedelghaish <kotbm055@gmail.com>
2024-06-23 13:11:53 +0300

    add get network function test

--- Shopify/ShopifyTests/ShopifyTests.swift
+++ Shopify/ShopifyTests/ShopifyTests.swift
@@ -9,28 +9,25 @@ import XCTest
 @testable import Shopify
 
 final class ShopifyTests: XCTestCase {
-
+    var brands : [SmartCollections]?
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testGetData(){
+        let expectation = expectation(description: "API data retrieval")
+        let url = URLs.shared.getBrandsURL()
+        
+        NetworkManger.shared.getData(url: url){ response in
+            self.brands = response?.smart_collections
+            XCTAssertNotNil(self.brands, "not nil")
+            XCTAssertEqual(self.brands?.count , 12)
+            expectation.fulfill()
         }
+        wait(for: [expectation], timeout: 20)
     }
-
 }
